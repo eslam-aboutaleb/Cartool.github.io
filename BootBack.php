0ab485f3c1394e5b9026246c4dc7271f801f07a2
Eslam Ehab <eslamaboutaleb@yahoo.com>
2020-11-28 17:08:26 +0200

    Add files via upload

--- BootBack.php
+++ BootBack.php
@@ -5,19 +5,18 @@ $passwords =array("admin");
 if(isset($_GET["UserName"]))
 {
 	$UserName = $_GET["UserName"];
-	if(isset($_GET["Userpassword"]))
-	{
-		$UserPass = $_GET["Userpassword"];
-		if((in_array($UserName , $admins)) && (in_array($UserPass , $passwords)))
-		{
-			header("Location:fileRecv.html");
-		}
-		else
-		{
-			header("Location:BootApp.html");
-		}
-		
-	}
-
+}
+if(isset($_GET["Userpassword"]))
+{
+	$UserPass = $_GET["Userpassword"];
 }
 
+if((in_array($UserName , $admins)) && (in_array($UserPass , $passwords)))
+{
+	header("Location:BootApp.html");
+}
+else
+{
+	header("Location:fileRecv.html");
+	
+}
